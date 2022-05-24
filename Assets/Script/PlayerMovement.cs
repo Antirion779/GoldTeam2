18d262241f7d7c9480a7feba35dd1388ea74d84e
Artyu24 <arthurpejoiane@hotmail.fr>
2022-05-25 00:25:05 +0200

    Add Wall Detection

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,8 +7,17 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 startPos;
     public int pixerDistToDetect = 20;
     private bool fingerDown;
-    public float moveSpeed = 5;
-    public float moveDistance = 1;
+    public float moveSpeed = 5, moveDistance = 1;
+
+    public LayerMask collisionLayer;
+    public float raycastDistance = 2;
+    private bool northCollision, southCollision, eastCollision, westCollision;
+
+    private void Start()
+    {
+        CheckWall();
+    }
+
 
     private void Update()
     {
@@ -16,29 +25,30 @@ public class PlayerMovement : MonoBehaviour
         {
             startPos = Input.touches[0].position;
             fingerDown = true;
+            CheckWall();
         }
 
         if (fingerDown)
         {
-            if (Input.touches[0].position.y >= startPos.y + pixerDistToDetect)
+            if (Input.touches[0].position.y >= startPos.y + pixerDistToDetect && !northCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position,new Vector3(transform.position.x, transform.position.y + moveDistance, transform.position.z), moveSpeed * Time.deltaTime);
                 //Debug.Log("Up");
             }
-            else if (Input.touches[0].position.y <= startPos.y - pixerDistToDetect)
+            else if (Input.touches[0].position.y <= startPos.y - pixerDistToDetect && !southCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position,new Vector3(transform.position.x, transform.position.y - moveDistance, transform.position.z), moveSpeed * Time.deltaTime);
                 //Debug.Log("Down");
             }
-            else if (Input.touches[0].position.x <= startPos.x - pixerDistToDetect)
+            else if (Input.touches[0].position.x <= startPos.x - pixerDistToDetect && !westCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - moveDistance, transform.position.y, transform.position.z), moveSpeed * Time.deltaTime);
                 //Debug.Log("Left");
             }
-            else if (Input.touches[0].position.x >= startPos.x + pixerDistToDetect)
+            else if (Input.touches[0].position.x >= startPos.x + pixerDistToDetect && !eastCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + moveDistance, transform.position.y, transform.position.z), moveSpeed * Time.deltaTime);
@@ -47,7 +57,9 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if (fingerDown && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended)
+        {
             fingerDown = false;
+        }
 
             //TESTING FOR PC
 
@@ -55,29 +67,30 @@ public class PlayerMovement : MonoBehaviour
         {
             startPos = Input.touches[0].position;
             fingerDown = true;
+            CheckWall();
         }
 
         if (fingerDown)
         {
-            if (Input.mousePosition.y >= startPos.y + pixerDistToDetect)
+            if (Input.mousePosition.y >= startPos.y + pixerDistToDetect && !northCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + moveDistance, transform.position.z), moveSpeed * Time.deltaTime);
                 //Debug.Log("Up");
             }
-            else if (Input.mousePosition.y <= startPos.y - pixerDistToDetect)
+            else if (Input.mousePosition.y <= startPos.y - pixerDistToDetect && !southCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y - moveDistance, transform.position.z), moveSpeed * Time.deltaTime);
                 //Debug.Log("Down");
             }
-            else if (Input.mousePosition.x <= startPos.x - pixerDistToDetect)
+            else if (Input.mousePosition.x <= startPos.x - pixerDistToDetect && !westCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - moveDistance, transform.position.y, transform.position.z), moveSpeed * Time.deltaTime);
                 //Debug.Log("Left");
             }
-            else if (Input.mousePosition.x >= startPos.x + pixerDistToDetect)
+            else if (Input.mousePosition.x >= startPos.x + pixerDistToDetect && !eastCollision)
             {
                 fingerDown = false;
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + moveDistance, transform.position.y, transform.position.z), moveSpeed * Time.deltaTime);
@@ -86,6 +99,43 @@ public class PlayerMovement : MonoBehaviour
         }
 
         if (fingerDown && Input.GetMouseButtonUp(0))
+        {
             fingerDown = false;
+        }
+
+        Debug.DrawRay(transform.position, transform.up * raycastDistance, Color.blue);
+        Debug.DrawRay(transform.position, -transform.up * raycastDistance, Color.blue);
+        Debug.DrawRay(transform.position, -transform.right * raycastDistance, Color.blue);
+        Debug.DrawRay(transform.position, transform.right * raycastDistance, Color.blue);
+    }
+
+    private void CheckWall()
+    {
+        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, transform.up, raycastDistance, collisionLayer);
+        if (hitUp.collider != null)
+            northCollision = true;
+        else
+            northCollision = false;
+            
+        
+        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, -transform.up, raycastDistance, collisionLayer);
+        if (hitDown.collider != null)
+            southCollision = true;
+        else
+            southCollision = false;
+        
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, -transform.right, raycastDistance, collisionLayer);
+        if (hitLeft.collider != null)
+            westCollision = true;
+        else
+            westCollision = false;
+
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, transform.right, raycastDistance, collisionLayer);
+        if (hitRight.collider != null)
+            eastCollision = true;
+        else
+            eastCollision = false;
+
+        //Debug.Log(northCollision + "/" + southCollision + "/" + westCollision + "/" + eastCollision);
     }
 }
