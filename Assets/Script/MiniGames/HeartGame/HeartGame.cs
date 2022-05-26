b8ebad094a35bb29eca69a584054383c01cb0b79
Julien <julien.aberton@edu.devinci.fr>
2022-05-26 16:24:02 +0200

    Clean code

--- Assets/Script/MiniGames/HeartGame/HeartGame.cs
+++ Assets/Script/MiniGames/HeartGame/HeartGame.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -7,54 +6,55 @@ public class HeartGame : MonoBehaviour
 {
     [SerializeField] GameObject gameUI;
     [SerializeField] TMP_Text _text;
-    [SerializeField] GameObject borderHeight;
-    [SerializeField] GameObject borderHeight2;
-    [SerializeField] List<GameObject> _HeartList;
-    [SerializeField] GameObject HeartPrefab;
-    [SerializeField] Transform HeartTransformSpawn;
+    [SerializeField] GameObject _borderHeight;
+    [SerializeField] GameObject _borderHeight2;
+    [SerializeField] List<GameObject> _heartList;
+    [SerializeField] GameObject _heartPrefab;
+    [SerializeField] Transform _heartTransformSpawn;
 
     [Header("Game Options")]
     [SerializeField] float _gameTime;
     [SerializeField] float _HeartSpeed;
     [SerializeField] float _HeartMinusInterval;
     [SerializeField] float _HeartMaxInterval;
-    private float _HeartInterval;
-    public float _HeartCurrentInterval;
-    private bool isGamePlaying = true;
+
+    private float _heartInterval;
+    private float _heartCurrentInterval;
+    private bool _isGamePlaying = true;
 
     private void Start()
     {
-        _HeartInterval = Random.Range(_HeartMinusInterval, _HeartMaxInterval);
-        _HeartCurrentInterval = _HeartInterval;
+        _heartInterval = Random.Range(_HeartMinusInterval, _HeartMaxInterval);
+        _heartCurrentInterval = _heartInterval;
     }
     // Update is called once per frame
     void Update()
     {
-        if(isGamePlaying)
+        if(_isGamePlaying)
         {
             _gameTime -= Time.deltaTime;
             _text.text = _gameTime.ToString("0.0");
 
             if (_gameTime < 0)
             {
-                isGamePlaying = false;
+                _isGamePlaying = false;
             }
 
-            _HeartCurrentInterval -= Time.deltaTime;
+            _heartCurrentInterval -= Time.deltaTime;
 
-            if(_HeartCurrentInterval < 0)
+            if(_heartCurrentInterval < 0)
             {
-                _HeartCurrentInterval = Random.Range(_HeartMinusInterval, _HeartMaxInterval);
+                _heartCurrentInterval = Random.Range(_HeartMinusInterval, _HeartMaxInterval);
                 GameObject go;
-                go = Instantiate(HeartPrefab, HeartTransformSpawn.position, Quaternion.identity) as GameObject;
-                go.transform.parent = HeartTransformSpawn;
+                go = Instantiate(_heartPrefab, _heartTransformSpawn.position, Quaternion.identity) as GameObject;
+                go.transform.parent = _heartTransformSpawn;
             }
 
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                foreach(GameObject heart in _HeartList)
+                foreach(GameObject heart in _heartList)
                 {
-                    if(heart.transform.position.y <= borderHeight.transform.position.y && heart.transform.position.y >= borderHeight2.transform.position.y)
+                    if(heart.transform.position.y <= _borderHeight.transform.position.y && heart.transform.position.y >= _borderHeight2.transform.position.y)
                     {
                         SelectHeart();
                     }
