71347970f76953774996d3007848201b1f5212e5
Julien <julien.aberton@edu.devinci.fr>
2022-05-27 11:28:54 +0200

    Update Bomb script

--- Assets/Script/GameFeatures/Bomb.cs
+++ Assets/Script/GameFeatures/Bomb.cs
@@ -1,22 +1,24 @@
 using UnityEngine;
+using UnityEditor;
 using System;
 public class Bomb : MonoBehaviour
 {
+    [Header("Variables")]
+    [SerializeField] GameObject _player;
+
     [Header("Bomb Options")]
     [SerializeField] int _actionPoint;
-    public enum NumberOfPlatform { _1 = 1, _2 = 2, _4 = 4 }
-    public NumberOfPlatform numberOfPlatform;
-
-    public enum PlatformAxis { HORIZONTAL, VERTICAL }
-    public PlatformAxis platformAxis;
 
     [SerializeField, Range(-30, 30)] int _moveOnX = 0;
     [SerializeField, Range(-30, 30)] int _moveOnY = 0;
+    public enum NumberOfPlatform { _1, _2, _4 }
+    public NumberOfPlatform numberOfPlatform;
+    private enum PlatformAxis { HORIZONTAL, VERTICAL }
+    private PlatformAxis platformAxis;
     private int _actionRemaining;
     private bool _hasBeenUsed = false;
 
-    [Header("Variables")]
-    [SerializeField] GameObject _player;
+
 
     private void Start()
     {
@@ -89,17 +91,17 @@ public class Bomb : MonoBehaviour
                 Gizmos.DrawCube(new Vector3(_moveOnX, _moveOnY, 0), new Vector3(1, 1, 1));
                 break;
             case Bomb.NumberOfPlatform._2:
-                if(platformAxis == Bomb.PlatformAxis.HORIZONTAL)
+                if (platformAxis == Bomb.PlatformAxis.HORIZONTAL)
                 {
                     Gizmos.DrawCube(new Vector3(_moveOnX, _moveOnY, 0), new Vector3(1, 1, 1));
                     Gizmos.DrawCube(new Vector3(_moveOnX + 1, _moveOnY, 0), new Vector3(1, 1, 1));
-                }          
+                }
                 else
                 {
                     Gizmos.DrawCube(new Vector3(_moveOnX, _moveOnY, 0), new Vector3(1, 1, 1));
                     Gizmos.DrawCube(new Vector3(_moveOnX, _moveOnY + 1, 0), new Vector3(1, 1, 1));
                 }
-                    
+
                 break;
             case Bomb.NumberOfPlatform._4:
                 Gizmos.DrawCube(new Vector3(_moveOnX, _moveOnY, 0), new Vector3(1, 1, 1));
@@ -111,6 +113,21 @@ public class Bomb : MonoBehaviour
                 Debug.LogError("Bomb event switch enter in default");
                 break;
 
+        }
+    }
+
+    [CustomEditor(typeof(Bomb))]
+    public class MyScriptEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var myScript = target as Bomb;
+
+            if (myScript.numberOfPlatform == Bomb.NumberOfPlatform._2)
+            {
+                myScript.platformAxis = (PlatformAxis)EditorGUILayout.EnumPopup("Platform Axis", myScript.platformAxis);
+            }
         }
     }
 }
