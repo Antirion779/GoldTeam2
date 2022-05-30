424a495316213b7d60334dada55d0b372d61bb4b
Artyu24 <arthurpejoiane@hotmail.fr>
2022-05-30 10:25:12 +0200

    Update BoxCenter.cs

--- Assets/Script/Utility/BoxCenter.cs
+++ Assets/Script/Utility/BoxCenter.cs
@@ -6,26 +6,6 @@ public class BoxCenter : MonoBehaviour
 {
     private void Start()
     {
-        //Vector2 startPos = transform.position;
-        //float halfBlockDistance = GameManager.Instance.GetMoveDistance / 2;
-        //transform.position = new Vector3((int)(startPos.x / halfBlockDistance) * halfBlockDistance, (int)(startPos.y / halfBlockDistance) * halfBlockDistance);
-
-        //if (transform.position.x % GameManager.Instance.GetMoveDistance == 0)
-        //{
-        //    if (startPos.x >= 0)
-        //        transform.position = new Vector3(transform.position.x + halfBlockDistance, transform.position.y);
-        //    else
-        //        transform.position = new Vector3(transform.position.x - halfBlockDistance, transform.position.y);
-        //}
-
-        //if (transform.position.y % GameManager.Instance.GetMoveDistance == 0)
-        //{
-        //    if(startPos.y >= 0)
-        //        transform.position = new Vector3(transform.position.x, transform.position.y + halfBlockDistance);
-        //    else
-        //        transform.position = new Vector3(transform.position.x, transform.position.y - halfBlockDistance);
-        //}
-
         Vector2 startPos = transform.position;
         float blockDistance = GameManager.Instance.GetMoveDistance;
         float halfBlockDistance = GameManager.Instance.GetMoveDistance / 2;
