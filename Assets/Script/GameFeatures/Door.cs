28252411853414e1a344bf3277f2fa511ff7c52d
Antirion <73186199+Antirion779@users.noreply.github.com>
2022-06-01 09:02:46 +0200

    [change w/ GameState]

--- Assets/Script/GameFeatures/Door.cs
+++ Assets/Script/GameFeatures/Door.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    [SerializeField]private GameObject EnemiePrefab;
     private GameObject realDoor;
     private bool isOpen;
     private bool hasToChange;
