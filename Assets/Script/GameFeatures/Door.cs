67358f6e0e10ef4a37ee5c9dabe2df411c07e8a8
Antirion <73186199+Antirion779@users.noreply.github.com>
2022-06-01 15:56:45 +0200

    [Enemie turn + start door]
    Enemie turn when he have finished to move
    start door

--- Assets/Script/GameFeatures/Door.cs
+++ Assets/Script/GameFeatures/Door.cs
@@ -6,33 +6,71 @@ public class Door : MonoBehaviour
 {
     [SerializeField]private GameObject EnemiePrefab;
     private GameObject realDoor;
-    private bool isOpen;
-    private bool hasToChange;
-    private Vector3 endPos;
+
+    private GameObject enemieSpawned;
+
+    [Header("Patern")]
+    [SerializeField] [Tooltip("To know where you are in the patrol")] private int paternNumber = 0;
+    [SerializeField] [Tooltip("")] private string[] patern;
+    [SerializeField] [Tooltip("")] private string[] patrolPatern;
+
 
     //ouvre -> sort -> patrouille -> ferme
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Vector3 _newDir = Vector3.RotateTowards();
-        //transform.rotation = Quaternion.LookRotation();
-    }
-
     public void Action()
     {
+        MakeAMove(patern, paternNumber);
 
+        if (paternNumber < patern.Length - 1)
+            paternNumber++;
+        else
+            paternNumber = 0;
     }
 
-    void ChangeDoor()
+    void MakeAMove(string[] _patern, int _paternNumber)
     {
-        if (isOpen && hasToChange)
-            endPos = new Vector3(transform.rotation.x, transform.rotation.y, 0);
+        switch (_patern[_paternNumber])
+        {
+            case "OD":
+                OpenDoor();
+                break;
+
+            case "CD":
+                CloseDoor();
+                break;
+
+            case "EN":
+                EnterInTheMap();
+                break;
+
+            case "EX":
+                ExitTheMap();
+                break;
+
+            case "W":
+                break;
+        }
     }
+
+    void OpenDoor()
+    {
+       //Open the door
+    }
+
+    void CloseDoor()
+    {
+        //close the door
+    }
+
+    void EnterInTheMap()
+    {
+        //enemieSpawned = Instantiate(EnemiePrefab);
+        //enemieSpawned.GetComponent<EnemieBase>().patern = patrolPatern;
+    }
+
+    void ExitTheMap()
+    {
+        //Destroy(enemieSpawned);
+    }
+
 }
