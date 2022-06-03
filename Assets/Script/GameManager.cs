3411ded23e867c818259787d523ede8ab6c2b128
Julien <julien.aberton@edu.devinci.fr>
2022-06-03 11:50:26 +0200

    Update doorLevel

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -70,8 +70,6 @@ public class GameManager : MonoBehaviour
 
         pillsText.text = nbrPills.ToString();
 
-        openDoor.SetActive(false);
-
         ldGrid = FindObjectOfType<Grid>();
         moveDistance = ldGrid.cellSize.x;
 
@@ -157,7 +155,8 @@ public class GameManager : MonoBehaviour
         peopleToHeal--;
         if (peopleToHeal == 0)
         {
-            openDoor.SetActive(true);
+            openDoor.layer = 0;
+            openDoor.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 
