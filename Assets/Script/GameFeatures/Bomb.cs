eec3ca8e59a7a3ed45143e8bcca53e2d07256c89
Artyu24 <arthurpejoiane@hotmail.fr>
2022-06-07 23:06:50 +0200

    Add a lot of Debug for help the GD + Add Oil Prefab

--- Assets/Script/GameFeatures/Bomb.cs
+++ Assets/Script/GameFeatures/Bomb.cs
@@ -5,14 +5,14 @@ using TMPro;
 public class Bomb : Event
 {
     [Header("Variables")]
-    [SerializeField] GameObject _player;
-    [SerializeField] Grid _grid;
-    [SerializeField] GameObject _warningCube;
-    [SerializeField] GameObject _groundBreak;
+    [SerializeField] GameObject player;
+    [SerializeField] Grid grid;
+    [SerializeField] GameObject warningCube;
+    [SerializeField] GameObject groundBreak;
 
     [Header("Bomb Options")]
     [Tooltip("Rounds before explosion, if you choose 3, there will be 2 rounds of warning and the bomb will explode next round")]
-    [SerializeField] int _actionPointAfterWarning;
+    [SerializeField] int actionPointAfterWarning;
     private int _currentActionPointAfterWarning = 1;
 
     [SerializeField, Range(-30, 30)] int _moveOnX = 0;
@@ -38,10 +38,15 @@ public class Bomb : Event
         _listCubePos.Clear();
         _listOfWarningCube.Clear();
 
-        _originalCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2, 0);
-        _rightCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2 + _grid.cellSize.x, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2);
-        _upCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2 + _grid.cellSize.y, 0);
-        _upRightCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2 + _grid.cellSize.x, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2 + _grid.cellSize.y, 0);
+        if(player == null || grid == null || actionPointAfterWarning == 0)
+            Debug.Log("<color=gray>[</color><color=#FF00FF>Bomb</color><color=gray>]</color><color=red> ATTENTION </color><color=#F48FB1> Some object are null </color><color=gray>-</color><color=cyan> Object Name : </color><color=yellow>" + transform.name + "</color><color=cyan> Player : </color><color=yellow>" + player + "</color><color=cyan> Grid : </color><color=yellow>" + grid + "</color><color=cyan> Action Point After Warning : </color><color=yellow>" + actionPointAfterWarning + "</color>");
+        else
+        {
+            _originalCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2, 0);
+            _rightCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2 + grid.cellSize.x, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2);
+            _upCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2 + grid.cellSize.y, 0);
+            _upRightCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2 + grid.cellSize.x, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2 + grid.cellSize.y, 0);
+        }
     }
 
 
@@ -86,15 +91,15 @@ public class Bomb : Event
         {
             if(go != null)
             {
-                go.GetComponent<SpriteRenderer>().color = new Color((float)ActionPoint / (float)_actionPointAfterWarning, 0, 0, 1);
+                go.GetComponent<SpriteRenderer>().color = new Color((float)ActionPoint / (float)actionPointAfterWarning, 0, 0, 1);
                 var textGo = go.transform.GetChild(0).GetChild(0).gameObject;
                 Debug.Log(textGo);
-                textGo.GetComponent<TMP_Text>().text = (_actionPointAfterWarning - _currentActionPointAfterWarning).ToString();              
+                textGo.GetComponent<TMP_Text>().text = (actionPointAfterWarning - _currentActionPointAfterWarning).ToString();              
             }
         }
         _currentActionPointAfterWarning++;
 
-        if (ActionPoint == _actionPointAfterWarning)
+        if (ActionPoint == actionPointAfterWarning)
         {
             LaunchBomb();
         }
@@ -116,8 +121,8 @@ public class Bomb : Event
     private void InstantiateWarningCube(Vector3 pos)
     {
         _listCubePos.Add(pos);
-        var go = Instantiate(_warningCube, pos, Quaternion.identity);
-        go.transform.localScale = new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1);
+        var go = Instantiate(warningCube, pos, Quaternion.identity);
+        go.transform.localScale = new Vector3(grid.cellSize.x, grid.cellSize.y, 1);
         _listOfWarningCube.Add(go);
     }
 
@@ -127,7 +132,7 @@ public class Bomb : Event
         switch (numberOfPlatform)
         {
             case Bomb.NumberOfPlatform._1:
-                if (Mathf.Abs(Vector3.Distance(_player.transform.position, _originalCubePos))  <= 0.1f ) 
+                if (Mathf.Abs(Vector3.Distance(player.transform.position, _originalCubePos))  <= 0.1f ) 
                 {
                     return true;
                 }
@@ -135,18 +140,18 @@ public class Bomb : Event
             case Bomb.NumberOfPlatform._2:
                 if (platformAxis == Bomb.PlatformAxis.HORIZONTAL)
                 {
-                    if (Mathf.Abs(Vector3.Distance(_player.transform.position, _originalCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(_player.transform.position, _rightCubePos)) <= 0.1f)
+                    if (Mathf.Abs(Vector3.Distance(player.transform.position, _originalCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(player.transform.position, _rightCubePos)) <= 0.1f)
                     {
                         return true;
                     }
                 }
-                else if (Mathf.Abs(Vector3.Distance(_player.transform.position, _originalCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(_player.transform.position, _upCubePos)) <= 0.1)
+                else if (Mathf.Abs(Vector3.Distance(player.transform.position, _originalCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(player.transform.position, _upCubePos)) <= 0.1)
                 {
                     return true;
                 }
                 return false;
             case Bomb.NumberOfPlatform._4:
-                if (Mathf.Abs(Vector3.Distance(_player.transform.position, _originalCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(_player.transform.position, _rightCubePos)) <= 0.1 || Mathf.Abs(Vector3.Distance(_player.transform.position, _upCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(_player.transform.position, _upRightCubePos)) <= 0.1)
+                if (Mathf.Abs(Vector3.Distance(player.transform.position, _originalCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(player.transform.position, _rightCubePos)) <= 0.1 || Mathf.Abs(Vector3.Distance(player.transform.position, _upCubePos)) <= 0.1f || Mathf.Abs(Vector3.Distance(player.transform.position, _upRightCubePos)) <= 0.1)
                 { 
                     return true;
                 }
@@ -168,48 +173,51 @@ public class Bomb : Event
 
         foreach (Vector3 vect in _listCubePos)
         {
-            var go = Instantiate(_groundBreak, vect, Quaternion.identity);
-            go.transform.localScale = new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1);
+            var go = Instantiate(groundBreak, vect, Quaternion.identity);
+            go.transform.localScale = new Vector3(grid.cellSize.x, grid.cellSize.y, 1);
         }
     }
 
     void OnDrawGizmos()
     {
-        _originalCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2, 0);
-        _rightCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2 + _grid.cellSize.x, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2);
-        _upCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2 + _grid.cellSize.y, 0);
-        _upRightCubePos = new Vector3(_moveOnX * _grid.cellSize.x + _grid.cellSize.x / 2 + _grid.cellSize.x, _moveOnY * _grid.cellSize.y + _grid.cellSize.y / 2 + _grid.cellSize.y, 0);
-
-        // Draw a semitransparent blue cube at the transforms position
-        Gizmos.color = new Color(1, 0, 0, 0.5f);
-        switch (numberOfPlatform)
+        if (grid != null)
         {
-            case Bomb.NumberOfPlatform._1:
-                Gizmos.DrawCube(_originalCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                break;
-            case Bomb.NumberOfPlatform._2:
-                if (platformAxis == Bomb.PlatformAxis.HORIZONTAL)
-                {
-                    Gizmos.DrawCube(_originalCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                    Gizmos.DrawCube(_rightCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                }
-                else
-                {
-                    Gizmos.DrawCube(_originalCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                    Gizmos.DrawCube(_upCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                }
+            _originalCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2, 0);
+            _rightCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2 + grid.cellSize.x, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2);
+            _upCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2 + grid.cellSize.y, 0);
+            _upRightCubePos = new Vector3(_moveOnX * grid.cellSize.x + grid.cellSize.x / 2 + grid.cellSize.x, _moveOnY * grid.cellSize.y + grid.cellSize.y / 2 + grid.cellSize.y, 0);
 
-                break;
-            case Bomb.NumberOfPlatform._4:
-                Gizmos.DrawCube(_originalCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                Gizmos.DrawCube(_rightCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                Gizmos.DrawCube(_upCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                Gizmos.DrawCube(_upRightCubePos, new Vector3(_grid.cellSize.x, _grid.cellSize.y, 1));
-                break;
-            default:
-                Debug.LogError("Bomb event switch enter in default");
-                break;
+            // Draw a semitransparent blue cube at the transforms position
+            Gizmos.color = new Color(1, 0, 0, 0.5f);
+            switch (numberOfPlatform)
+            {
+                case Bomb.NumberOfPlatform._1:
+                    Gizmos.DrawCube(_originalCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                    break;
+                case Bomb.NumberOfPlatform._2:
+                    if (platformAxis == Bomb.PlatformAxis.HORIZONTAL)
+                    {
+                        Gizmos.DrawCube(_originalCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                        Gizmos.DrawCube(_rightCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                    }
+                    else
+                    {
+                        Gizmos.DrawCube(_originalCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                        Gizmos.DrawCube(_upCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                    }
 
+                    break;
+                case Bomb.NumberOfPlatform._4:
+                    Gizmos.DrawCube(_originalCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                    Gizmos.DrawCube(_rightCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                    Gizmos.DrawCube(_upCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                    Gizmos.DrawCube(_upRightCubePos, new Vector3(grid.cellSize.x, grid.cellSize.y, 1));
+                    break;
+                default:
+                    Debug.LogError("Bomb event switch enter in default");
+                    break;
+
+            }
         }
     }
 }
